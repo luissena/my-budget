0cdc6daa5be2fd291533cdda512813654d6828c5
luissena <luis.senarode@gmail.com>
2024-02-19 14:59:29 -0300

    feat: add categoryId in transaction

--- server/src/domain/enterprise/entities/transaction.ts
+++ server/src/domain/enterprise/entities/transaction.ts
@@ -1,51 +1,58 @@
-import { Entity } from "@/core/entities/entity";
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 export interface TransactionProps {
-    title: string;
-    amount: number;
-    date: Date;
-    userId: UniqueEntityID;
+  title: string
+  amount: number
+  date: Date
+  userId: UniqueEntityID
+  categoryId: UniqueEntityID
 }
 
 export class Transaction extends Entity<TransactionProps> {
+  get title() {
+    return this.props.title
+  }
 
-    get title() {
-        return this.props.title;
-    }
+  set title(value: string) {
+    this.props.title = value
+  }
 
-    set title(value: string) {
-        this.props.title = value;
-    }
+  get amount() {
+    return this.props.amount
+  }
 
-    get amount() {
-        return this.props.amount;
-    }
+  set amount(value: number) {
+    this.props.amount = value
+  }
 
-    set amount(value: number) {
-        this.props.amount = value;
-    }
+  get date() {
+    return this.props.date
+  }
 
-    get date() {
-        return this.props.date;
-    }
+  set date(value: Date) {
+    this.props.date = value
+  }
 
-    set date(value: Date) {
-        this.props.date = value;
-    }
+  get categoryId() {
+    return this.props.categoryId
+  }
 
-    get userId() {
-        return this.props.userId;
-    }
+  set categoryId(value: UniqueEntityID) {
+    this.props.categoryId = value
+  }
 
-    set userId(value: UniqueEntityID) {
-        this.props.userId = value;
-    }
+  get userId() {
+    return this.props.userId
+  }
 
+  set userId(value: UniqueEntityID) {
+    this.props.userId = value
+  }
 
   static create(props: TransactionProps, id?: UniqueEntityID) {
-    const transaction = new Transaction(props, id);
+    const transaction = new Transaction(props, id)
 
-    return transaction;
+    return transaction
   }
 }
