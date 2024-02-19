3724e712fa3b0f0368c46faf61ab8041af0dad3c
luissena <luis.senarode@gmail.com>
2024-02-19 14:58:45 -0300

    feat: findById categories repository

--- server/src/domain/application/repositories/categories-repository.ts
+++ server/src/domain/application/repositories/categories-repository.ts
@@ -1,5 +1,6 @@
-import { Category } from "@/domain/enterprise/entities/category";
+import { Category } from '@/domain/enterprise/entities/category'
 
 export abstract class CategoriesRepository {
-  abstract create(category: Category): Promise<void>;
+  abstract create(category: Category): Promise<void>
+  abstract findById(id: string): Promise<Category | null>
 }
