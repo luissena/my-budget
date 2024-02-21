dc06c5037bb5f9303406418a40d0e0747ff34ad5
luissena <luis.senarode@gmail.com>
2024-02-21 15:56:23 -0300

    feat: authenticate

--- server/src/domain/application/use-cases/authenticate-user.ts
+++ server/src/domain/application/use-cases/authenticate-user.ts
@@ -1,20 +1,20 @@
-import { Either, left, right } from '@/core/either';
-import { Injectable } from '@nestjs/common';
-import { Encrypter } from '../cryptography/encrypter';
-import { HashComparer } from '../cryptography/hash-comparer';
-import { UsersRepository } from '../repositories/users-repository';
-import { WrongCredentialsError } from './errors/wrong-credentials-error';
+import { Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import { Encrypter } from '../cryptography/encrypter'
+import { HashComparer } from '../cryptography/hash-comparer'
+import { UsersRepository } from '../repositories/users-repository'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
 
 interface AuthenticateUserUseCaseRequest {
-  email: string;
-  password: string;
+  email: string
+  password: string
 }
 type AuthenticateUserUseCaseResponse = Either<
   WrongCredentialsError,
   {
-    accessToken: string;
+    accessToken: string
   }
->;
+>
 
 @Injectable()
 export class AuthenticateUserUseCase {
@@ -25,24 +25,24 @@ export class AuthenticateUserUseCase {
   ) {}
 
   async execute({ email, password }: AuthenticateUserUseCaseRequest): Promise<AuthenticateUserUseCaseResponse> {
-    const user = await this.usersRepository.findByEmail(email);
+    const user = await this.usersRepository.findByEmail(email)
 
     if (!user) {
-      throw left(new WrongCredentialsError());
+      return left(new WrongCredentialsError())
     }
 
-    const isPasswordValid = await this.hashComparer.compare(password, user.password);
+    const isPasswordValid = await this.hashComparer.compare(password, user.password)
 
     if (!isPasswordValid) {
-      return left(new WrongCredentialsError());
+      return left(new WrongCredentialsError())
     }
 
     const accessToken = await this.encrypter.encrypt({
       sub: user.id.toString(),
-    });
+    })
 
     return right({
       accessToken,
-    });
+    })
   }
 }
--- server/src/infra/http/http.module.ts
+++ server/src/infra/http/http.module.ts
@@ -1,12 +1,14 @@
+import { AuthenticateUserUseCase } from '@/domain/application/use-cases/authenticate-user'
 import { RegisterUserUseCase } from '@/domain/application/use-cases/register-user'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { Module } from '@nestjs/common'
 import { CryptographyModule } from '../cryptography/cryptography.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
-  controllers: [CreateAccountController],
-  providers: [RegisterUserUseCase],
+  controllers: [CreateAccountController, AuthenticateController],
+  providers: [RegisterUserUseCase, AuthenticateUserUseCase],
 })
 export class HttpModule {}
