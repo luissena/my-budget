56061986b5e16fa3acd4f7668f5bd9f1860256b2
luissena <luis.senarode@gmail.com>
2024-02-23 16:05:57 -0300

    fix: deferredcontent theme

--- client/presets/lara/index.js
+++ client/presets/lara/index.js
@@ -1,159 +1,157 @@
-import global from './global.js';
-import autocomplete from './autocomplete';
-import calendar from './calendar';
-import cascadeselect from './cascadeselect';
-import checkbox from './checkbox';
-import chips from './chips';
-import colorpicker from './colorpicker';
-import dropdown from './dropdown';
-import inputgroup from './inputgroup';
-import inputgroupaddon from './inputgroupaddon';
-import inputmask from './inputmask';
-import inputnumber from './inputnumber';
-import inputswitch from './inputswitch';
-import inputtext from './inputtext';
-import knob from './knob';
-import listbox from './listbox';
-import multiselect from './multiselect';
-import password from './password';
-import radiobutton from './radiobutton';
-import rating from './rating';
-import selectbutton from './selectbutton';
-import slider from './slider';
-import textarea from './textarea';
-import togglebutton from './togglebutton';
-import treeselect from './treeselect';
-import tristatecheckbox from './tristatecheckbox';
-import button from './button';
-import speeddial from './speeddial';
-import splitbutton from './splitbutton';
-import datatable from './datatable';
-import dataview from './dataview';
-import orderlist from './orderlist';
-import organizationchart from './organizationchart';
-import paginator from './paginator';
-import picklist from './picklist';
-import tree from './tree';
-import treetable from './treetable';
-import timeline from './timeline';
-import message from './message';
-import inlinemessage from './inlinemessage';
-import toast from './toast';
-import image from './image';
-import accordion from './accordion';
-import card from './card';
-import deferredcontent from './deferredcontent';
-import divider from './divider';
-import fieldset from './fieldset';
-import panel from './panel';
-import scrollpanel from './scrollpanel';
-import tabview from './tabview';
-import toolbar from './toolbar';
-import confirmpopup from './confirmpopup';
-import dialog from './dialog';
-import overlaypanel from './overlaypanel';
-import sidebar from './sidebar';
-import tooltip from './tooltip';
-import breadcrumb from './breadcrumb';
-import contextmenu from './contextmenu';
-import dock from './dock';
-import menu from './menu';
-import menubar from './menubar';
-import megamenu from './megamenu';
-import panelmenu from './panelmenu';
-import steps from './steps';
-import tabmenu from './tabmenu';
-import tieredmenu from './tieredmenu';
-import avatar from './avatar';
-import badge from './badge';
-import badgedirective from './badgedirective';
-import blockui from './blockui';
-import chip from './chip';
-import scrolltop from './scrolltop';
-import skeleton from './skeleton';
-import progressbar from './progressbar';
-import ripple from './ripple';
-import tag from './tag';
-import terminal from './terminal';
+import accordion from "./accordion";
+import autocomplete from "./autocomplete";
+import avatar from "./avatar";
+import badge from "./badge";
+import badgedirective from "./badgedirective";
+import blockui from "./blockui";
+import breadcrumb from "./breadcrumb";
+import button from "./button";
+import calendar from "./calendar";
+import card from "./card";
+import cascadeselect from "./cascadeselect";
+import checkbox from "./checkbox";
+import chip from "./chip";
+import chips from "./chips";
+import colorpicker from "./colorpicker";
+import confirmpopup from "./confirmpopup";
+import contextmenu from "./contextmenu";
+import datatable from "./datatable";
+import dataview from "./dataview";
+import dialog from "./dialog";
+import divider from "./divider";
+import dock from "./dock";
+import dropdown from "./dropdown";
+import fieldset from "./fieldset";
+import global from "./global.js";
+import image from "./image";
+import inlinemessage from "./inlinemessage";
+import inputgroup from "./inputgroup";
+import inputgroupaddon from "./inputgroupaddon";
+import inputmask from "./inputmask";
+import inputnumber from "./inputnumber";
+import inputswitch from "./inputswitch";
+import inputtext from "./inputtext";
+import knob from "./knob";
+import listbox from "./listbox";
+import megamenu from "./megamenu";
+import menu from "./menu";
+import menubar from "./menubar";
+import message from "./message";
+import multiselect from "./multiselect";
+import orderlist from "./orderlist";
+import organizationchart from "./organizationchart";
+import overlaypanel from "./overlaypanel";
+import paginator from "./paginator";
+import panel from "./panel";
+import panelmenu from "./panelmenu";
+import password from "./password";
+import picklist from "./picklist";
+import progressbar from "./progressbar";
+import radiobutton from "./radiobutton";
+import rating from "./rating";
+import ripple from "./ripple";
+import scrollpanel from "./scrollpanel";
+import scrolltop from "./scrolltop";
+import selectbutton from "./selectbutton";
+import sidebar from "./sidebar";
+import skeleton from "./skeleton";
+import slider from "./slider";
+import speeddial from "./speeddial";
+import splitbutton from "./splitbutton";
+import steps from "./steps";
+import tabmenu from "./tabmenu";
+import tabview from "./tabview";
+import tag from "./tag";
+import terminal from "./terminal";
+import textarea from "./textarea";
+import tieredmenu from "./tieredmenu";
+import timeline from "./timeline";
+import toast from "./toast";
+import togglebutton from "./togglebutton";
+import toolbar from "./toolbar";
+import tooltip from "./tooltip";
+import tree from "./tree";
+import treeselect from "./treeselect";
+import treetable from "./treetable";
+import tristatecheckbox from "./tristatecheckbox";
 
 export default {
-    global,
-    directives: {
-        tooltip,
-        badgedirective,
-        ripple
-    },
-    autocomplete,
-    calendar,
-    cascadeselect,
-    checkbox,
-    chips,
-    colorpicker,
-    dropdown,
-    inputgroup,
-    inputgroupaddon,
-    inputmask,
-    inputnumber,
-    inputswitch,
-    inputtext,
-    knob,
-    listbox,
-    multiselect,
-    password,
-    radiobutton,
-    rating,
-    selectbutton,
-    slider,
-    textarea,
-    togglebutton,
-    treeselect,
-    tristatecheckbox,
-    button,
-    speeddial,
-    splitbutton,
-    datatable,
-    dataview,
-    orderlist,
-    organizationchart,
-    paginator,
-    picklist,
-    tree,
-    treetable,
-    timeline,
-    message,
-    inlinemessage,
-    toast,
-    image,
-    accordion,
-    card,
-    deferredcontent,
-    divider,
-    fieldset,
-    panel,
-    scrollpanel,
-    tabview,
-    toolbar,
-    confirmpopup,
-    dialog,
-    overlaypanel,
-    sidebar,
-    breadcrumb,
-    contextmenu,
-    dock,
-    menu,
-    menubar,
-    megamenu,
-    panelmenu,
-    steps,
-    tabmenu,
-    tieredmenu,
-    avatar,
-    badge,
-    blockui,
-    chip,
-    scrolltop,
-    skeleton,
-    progressbar,
-    tag,
-    terminal
-}
+  global,
+  directives: {
+    tooltip,
+    badgedirective,
+    ripple,
+  },
+  autocomplete,
+  calendar,
+  cascadeselect,
+  checkbox,
+  chips,
+  colorpicker,
+  dropdown,
+  inputgroup,
+  inputgroupaddon,
+  inputmask,
+  inputnumber,
+  inputswitch,
+  inputtext,
+  knob,
+  listbox,
+  multiselect,
+  password,
+  radiobutton,
+  rating,
+  selectbutton,
+  slider,
+  textarea,
+  togglebutton,
+  treeselect,
+  tristatecheckbox,
+  button,
+  speeddial,
+  splitbutton,
+  datatable,
+  dataview,
+  orderlist,
+  organizationchart,
+  paginator,
+  picklist,
+  tree,
+  treetable,
+  timeline,
+  message,
+  inlinemessage,
+  toast,
+  image,
+  accordion,
+  card,
+  divider,
+  fieldset,
+  panel,
+  scrollpanel,
+  tabview,
+  toolbar,
+  confirmpopup,
+  dialog,
+  overlaypanel,
+  sidebar,
+  breadcrumb,
+  contextmenu,
+  dock,
+  menu,
+  menubar,
+  megamenu,
+  panelmenu,
+  steps,
+  tabmenu,
+  tieredmenu,
+  avatar,
+  badge,
+  blockui,
+  chip,
+  scrolltop,
+  skeleton,
+  progressbar,
+  tag,
+  terminal,
+};
